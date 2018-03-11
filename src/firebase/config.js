0525bb093e9f2d66ea35b6e95e13e215ebceb201
onsihi024 <onishi.shoichiro@tis.co.jp>
2018-03-12 03:27:21 +0900

    second commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyClDAvx2mbQ0WkMmkFnIt8a--VZVsbfujQ",
+    authDomain: "testproject-ac762.firebaseapp.com",
+    databaseURL: "https://testproject-ac762.firebaseio.com",
+    projectId: "testproject-ac762",
+    storageBucket: "testproject-ac762.appspot.com",
+    messagingSenderId: "77431513403"
+}
